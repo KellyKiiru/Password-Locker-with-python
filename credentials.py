a6cc31d2797f6d0d2c76326551fa1f1acacdc5a3
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-25 13:00:03 +0300

    create empty lists for user and credentials; initialize the attributes for both classes

--- credentials.py
+++ credentials.py
@@ -1,2 +1,11 @@
 class Credentials:
-    pass
+    
+    credentials_list = []
+    
+    def __init__(self, account, user_name, password):
+        self.account = account
+        self.user_name = user_name
+        self.password = password
+        
+    def save_credential(self):
+        pass
