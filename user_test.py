a55a6322665a39da05dd072e9359f12bba8b29e6
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-25 13:57:22 +0300

    add test to check if user is added to list

--- user_test.py
+++ user_test.py
@@ -13,6 +13,11 @@ class TestUser(unittest.TestCase):
     def test_initialization(self):
         self.assertEqual(self.new_user.username, "kelly")
         self.assertEqual(self.new_user.password, "asdf1234")
+    def test_save_user(self):
+        self.new_user = User("kelly", "asdf1234")   
+        self.new_user.save_user()
+        
+        self.assertEqual(len(User.user_list),1)     
 
 
 if __name__ == '__main__':
